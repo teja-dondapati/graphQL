e2db025363556769fbd426abfdb67bb463f6f49f
teja dondapati <tejadondapati1@gmail.com>
2024-08-13 23:06:27 -0600

    initial commit

---
+++ src/main/java/com/example/demo/GraphQlServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/BookControllerTests.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+import java.sql.SQLOutput;
+
+@GraphQlTest(BookController.class)
+public class BookControllerTests {
+
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @Test
+    void shouldGetFirstBook() {
+        this.graphQlTester
+                .documentName("bookDetails")
+                .variable("id", "book-1")
+                .execute()
+                .path("bookById")
+                .matchesJson("""
+                    {
+                        "id": "book-1",
+                        "name": "Effective Java",
+                        "pageCount": 416,
+                        "author": {
+                          "firstName": "Joshua",
+                          "lastName": "Bloch"
+                        }
+                    }
+                """);
+        System.out.println("All tests passed!");
+    }
+}
